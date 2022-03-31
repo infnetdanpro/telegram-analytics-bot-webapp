d5ba03cac640d1cdaf2082c35cabed0073e62a2b
infnetdanpro <33713163+infnetdanpro@users.noreply.github.com>
2022-03-31 22:53:20 +0200

    temp fix

--- views.py
+++ views.py
@@ -11,7 +11,7 @@ TOKEN = config["TELEGRAM_TOKEN"]
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.reply_to(message, f"Hello, {config.get('REDIS_URL')}" + message.from_user.first_name)
+    bot.reply_to(message, f"Hello, {config.get('')}" + message.from_user.first_name)
 
 
 @bot.message_handler(content_types=["document"])
@@ -22,6 +22,7 @@ def handle_docs(message):
 
     # in memory
     file = json.loads(downloaded_file.decode("utf-8"))
+    print(file)
 
     task = run_task.delay(reply_to=message.from_user.id, data_dict=file)
     bot.reply_to(
