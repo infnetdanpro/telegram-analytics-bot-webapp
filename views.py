7d0f62a5bd763f9fb5fad05b49dc717b541b8092
infnetdanpro <33713163+infnetdanpro@users.noreply.github.com>
2022-03-31 22:44:51 +0200

    Update views.py

--- views.py
+++ views.py
@@ -11,7 +11,7 @@ TOKEN = config["TELEGRAM_TOKEN"]
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.reply_to(message, "Hello, " + message.from_user.first_name)
+    bot.reply_to(message, f"Hello, {config.get('REDIS_URL')}" + message.from_user.first_name)
 
 
 @bot.message_handler(content_types=["document"])
